bcc3fa04c5de8b0db2bd24dbbf688ac3bb46cca6
Ramon Lopez <ramon.lopez@aon.at>
2014-11-02 12:55:09 +0100

    Aufgabe2 seperateWords check

--- Aufgabe2.hs
+++ Aufgabe2.hs
@@ -13,7 +13,20 @@ maxWordLength a = fromIntegral $ maximum $ map length $ concat $ separateWords a
 separateWords :: Text -> [[String]]
 separateWords a = map words a
 
---numWordOcc --TODO
+
+--numWordOcc
+--Der Text wird erst in einzelne Wörter aufgeteilt.
+--Dann werden die verschachtelten Listen verflacht mit concat (Lines sind hier uninteressant)
+--Duplikate von Wörtern entfernen
+--Liste von Wörtern filtern die b lang sind
+--Ausgabe der Länge der Liste
+numWordOcc :: Text -> Integer -> Integer
+numWordOcc a b = fromIntegral $ length $ filter (\x -> length x == (fromInteger b)) (removeDuplicates $ concat $ separateWords a)
+
+removeDuplicates :: [String] -> [String]
+removeDuplicates = foldl (\seen x -> if x `elem` seen
+                                      then seen
+                                      else seen ++ [x]) []
 
 --reverseCheck
 reverseCheck::Text -> Bool
@@ -24,5 +37,9 @@ compareTuple :: (String,String) -> Bool
 compareTuple a = if fst a == snd a then True
 	else False
 
---formatText
-formatText :: Text 
+{- formatText 
+
+formatText :: Text -> Integer -> Text
+formatText a i = if length (tail a) <= i then a
+	else head a
+-}
